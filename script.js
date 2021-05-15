1cc4dae882416c3d1bca6ffff3fec407ebddec18
William Nogueira <will.ribeiro@live.com>
2021-05-15 20:38:08 -0300

    alterações mobile

--- script.js
+++ script.js
@@ -110,8 +110,19 @@ document.querySelector('.pizzaInfo--addButton').addEventListener('click', ()=> {
     closeModal();
 })
 
+document.querySelector('.menu-openner').addEventListener('click', () => {
+    if(cart.length > 0) {
+        document.querySelector('aside').style.left = '0';
+    }
+})
+document.querySelector('.menu-closer').addEventListener('click', () => {
+        document.querySelector('aside').style.left = '100vw';
+})
+
 /*** CARRINHO ***/
 function updateCart() {
+    document.querySelector('.menu-openner span').innerHTML = cart.length;
+
     if(cart.length > 0) {
         document.querySelector('aside').classList.add('show');
         document.querySelector('.cart').innerHTML = '';
@@ -170,5 +181,6 @@ function updateCart() {
         document.querySelector('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
     } else {
         document.querySelector('aside').classList.remove('show')
+        document.querySelector('aside').style.left = '100vw'
     }
 }
